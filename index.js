2389d7a03e3c7f2f0bce4a2e3db8118c26e07ddc
augierakow <augierakow@gmail.com>
2023-10-09 23:23:31 +0000

    Remove initial 'Hello' before @username

--- index.js
+++ index.js
@@ -17,13 +17,14 @@ const boltApp = new App({
   token: botToken
 });
 
-// Listen for a message that contains "gpt"
-boltApp.message(/gpt (.+)/, async ({ message, say, context }) => {
-  const userQuery = context.matches[1]; // Extract text after "gpt"
+// Listen for any message
+boltApp.message(async ({ message, say }) => {
+  const userQuery = message.text; // Extract the entire message text
   const gptResponse = await fetchOpenAIResponse(userQuery); // Pass it to fetchOpenAIResponse
-  await say(`Hello, <@${message.user}>, ${gptResponse}`);
+  await say(`<@${message.user}>, ${gptResponse}`);
 });
 
+// Start the Bolt App
 (async () => {
   await boltApp.start(process.env.PORT || 3002);
   console.log(`⚡️ Bolt app is running!`);
@@ -33,36 +34,36 @@ boltApp.message(/gpt (.+)/, async ({ message, say, context }) => {
 const expressApp = express();
 const port = 3003;
 
-// This function fetches a response from OpenAI's GPT-4 model
-async function fetchOpenAIResponse(userQuery) {  // Added userQuery as a parameter
+// Function to fetch OpenAI Response
+async function fetchOpenAIResponse(userQuery) {
   try {
     const openai = new OpenAIApi(new Configuration({ apiKey: process.env.OPENAI_API_KEY }));
     const promptMessage = [
-      { role: "user", content: userQuery }  // Replaced "engineering aphorism" with userQuery
+      { role: "user", content: userQuery }
     ];
     const response = await openai.createChatCompletion({
       model: "gpt-4",
       messages: promptMessage
     });
-    return response.data.choices[0].message.content;  // Return the API response
+    return response.data.choices[0].message.content;
   } catch (error) {
     console.error("OpenAI API Error:", error);
-    return "An error occurred while fetching data from OpenAI";  // Return an error message
+    return "An error occurred while fetching data from OpenAI";
   }
 }
 
+// Express Test Route
 expressApp.get("/", async (req, res) => {
-  console.log('API Key:', process.env.OPENAI_API_KEY);
-
   try {
-    const response = await fetchOpenAIResponse("Test Query");  // Added a test query for the express route
-    res.send(response);  // Send the OpenAI API response as the HTTP response
+    const response = await fetchOpenAIResponse("Test Query");
+    res.send(response);
   } catch (error) {
     console.error("Error details:", error);
     res.status(500).send("An error occurred");
   }
 });
 
+// Start the Express App
 expressApp.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
